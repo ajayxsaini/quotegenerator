f7bae2128ec68880f0501172564d491b3343e1cc
Ajay Saini <ajaysaini74700@gmail.com>
2024-10-20 15:59:00 +0530

    Made a quote generator using react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
+const apiUrl = 'https://api.adviceslip.com/advice';
+
+//{"slip": { "id": 33, "advice": "Don't let the bastards grind you down."}}
 function App() {
+  const [advice, setAdvice] = useState('Please click button to see your advice!!')
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleQuoteGenerate = async () => {
+    setIsLoading(true)
+    const response = await axios.get(apiUrl)
+    setAdvice(response.data.slip.advice)
+    setIsLoading(false)
+    // console.log(response.data);
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Quote Generator</h1>
+      <section className='quote-container'>
+        { isLoading ? ( 
+          <p className='quote'>...</p>
+        ) : (
+        <blockquote className='quote'>{advice}</blockquote>
+        )}
+      </section>
+      <button className='default-btn' onClick={handleQuoteGenerate}> Give Me Advice</button>
+
     </div>
   );
 }
